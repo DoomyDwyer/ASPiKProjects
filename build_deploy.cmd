ea192ed9c2f2f3b71530d5c09afcd4b4e0f37624
DoomyDwyer <doomy@puscii.nl>
2021-12-06 19:42:10 +0000

    build_deploy.cmd (Windows) only runs cmake if sln file doesn't already exist - saves regenerating guids and essentially creating a new Plugin each time a project is built

--- build_deploy.cmd
+++ build_deploy.cmd
@@ -1,10 +1,11 @@
-cmake -G"Visual Studio 16 2019" ../
-
 set target=%1
 IF %1.==. set target=Release
 
 for %%I in (..) do set project=%%~nxI
 echo %%~nxI
 
-msbuild %project%.sln /p:Configuration=%target%
+set projectFile=%project%.sln
+IF NOT EXIST %projectFile% cmake -G"Visual Studio 16 2019" ../
+
+msbuild %projectFile% /p:Configuration=%target%
 copy /y VST3\%target%\%project%.vst3\Contents\x86_64-win\%project%.vst3 D:\VST3
